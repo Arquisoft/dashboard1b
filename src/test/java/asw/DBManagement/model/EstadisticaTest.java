58c809bf9b109e5c4fc24b04edeb404caafd8b6d
Javier Valduvieco García <javivaldu@gmail.com>
2017-04-04 00:23:02 +0200

    más tests

---
+++ src/test/java/asw/DBManagement/model/EstadisticaTest.java
@@ -0,0 +1,57 @@
+package asw.DBManagement.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import asw.Application;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+@Transactional
+public class EstadisticaTest {
+	
+private Estadistica est;
+private String titulo;
+private List<String> c;
+private List<Double> v;
+
+@Before
+public void setUp() throws Exception {
+	titulo  = "pedazoTitulo";
+	c  = new ArrayList<String>();
+	v  = new ArrayList<Double>();
+	c.add("campo1");
+	c.add("campo2");
+	v.add(23.0);
+	v.add(17.4);
+	
+	est = new Estadistica(titulo,c,v);
+}
+@After
+public void tearDown() throws Exception {
+
+}
+@Test
+public void getTitulo() throws Exception {
+    assertThat(est.getTitulo()).isEqualTo(titulo);
+}
+@Test
+public void setTitulo() throws Exception{
+	est.setTitulo("Titulo2");
+	assertThat(est.getTitulo()).isEqualTo("Titulo2");
+}
+
+
+
+}
