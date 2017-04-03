261ff94f68b718ece60384defccc2665bceb527b
Javier Valduvieco García <javivaldu@gmail.com>
2017-04-03 18:32:45 +0200

    para Pablo

--- src/test/java/asw/cucumber/steps/loginStepsSinprivilegios.java
+++ src/test/java/asw/cucumber/steps/loginStepsSinprivilegios.java
@@ -3,6 +3,7 @@ package asw.cucumber.steps;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -11,7 +12,12 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.WebApplicationContext;
 
 import asw.util.SeleniumUtils;
 import cucumber.api.DataTable;
@@ -26,22 +32,24 @@ import cucumber.api.java.en.When;
 
 public class loginStepsSinprivilegios {
   
-	 private WebDriver driver;
+	 private WebDriver driver =  new HtmlUnitDriver();
 	  private String baseUrl;
 	  private List<Map<String, String>> usuarios;
-	  
+	  @Autowired
+		WebApplicationContext context;
  @Before
  public void setUp() throws Exception {
-	    driver = new HtmlUnitDriver();
+	   
 	    usuarios = new ArrayList<>();
 	    baseUrl = "http://localhost:8090/";
-	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	    driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
 	    Map<String,String> Benji = new HashMap<String, String>();
 	    Benji.put("email","nakamura@gmail.com");
 	    Benji.put("password","123456");
 	    usuarios.add(Benji);
 	    
 	  }
+
 	@Given("^una lista de usuarios:$")
 	 public void una_lista_de_usuarios(List<Map<String,String>>arg1)throws Throwable {
 		  
@@ -56,18 +64,25 @@ public class loginStepsSinprivilegios {
 	  	boolean esta = false;
 	  	
 		for (Map<String, String> map : usuarios) {
+			
 			if (map.get("email").equals(arg1) && map.get("password").equals(arg2)) {
 				esta = true;
 			}
 		}
 		assertEquals(true, esta);
-		System.out.println("PASA POR AQUI ASFNASÑGUFJ");
-		 driver.findElement(By.name("email")).clear();
-		 driver.findElement(By.name("email")).sendKeys("nakamura@gmail.com");
-		 driver.findElement(By.name("password")).clear();
-		 driver.findElement(By.name("password")).sendKeys("123456");
-		 driver.findElement(By.cssSelector("input[type=\"submit\"]")).click();
-		 driver.findElement(By.id("logearse")).click();
+		WebDriverWait wait = new WebDriverWait(driver,10);// 10segundos towapos.
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("email")));
+		
+		driver.findElement(By.name("email")).clear();
+		
+	    driver.findElement(By.name("email")).sendKeys(arg1);
+	    driver.findElement(By.name("password")).clear();
+	    driver.findElement(By.name("password")).sendKeys(arg2);
+	    driver.findElement(By.cssSelector("input[type=\"submit\"]")).click();
+	    driver.findElement(By.id("logearse")).click();
+	    
+	    String texto = "USUARIO SIN PRIVILEGIOS";
+	    SeleniumUtils.textoPresentePagina(driver, texto);
 		 //se supone que estoy logeado.
 	   
 	}
